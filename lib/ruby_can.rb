64d4da8132d9819854a482c26baeaecb39fd68bf
Tomek Rabczak <tomek.rabczak@gmail.com>
2015-07-07 16:35:52 -1000

    seems like it's working

--- lib/ruby_can.rb
+++ lib/ruby_can.rb
@@ -10,5 +10,6 @@ puts "Waiting..."
 while true do
   can_packet = CanPacket.read(socket.read(16))
 
-  puts can_packet.inspect
+  hex_data_output = can_packet.data.map{|b| b.to_i.to_s(16) }.join(' ')
+  puts "can_id: #{ can_packet.can_id }, can_dlc: #{ can_packet.can_dlc }, data: #{ hex_data_output }"
 end
--- lib/ruby_can/can_packet.rb
+++ lib/ruby_can/can_packet.rb
@@ -4,7 +4,8 @@ class CanPacket < BinData::Record
   endian :little
 
   uint32 :can_id
-  uint8  :can_dlc
-  array  :data, :initial_length => :can_dlc
+  uint32  :can_dlc
+  array  :data, :type => :uint8, :initial_length => :can_dlc, :read_length => :can_dlc
+  #string :data, length: 8
 end
 
